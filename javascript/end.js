aaae98d4a9c488ebbfa5a1877860da7b6ea7abd5
burnjoe <sabanajholo@gmail.com>
2024-04-02 07:29:29 +0800

    add number of correct answers counter

--- javascript/end.js
+++ javascript/end.js
@@ -1,20 +1,24 @@
-const username = localStorage.getItem('playerName')
-const saveScoreBtn = document.querySelector('#saveScoreBtn')
-const finalScore = document.querySelector('#finalScore')
-const mostRecentScore= localStorage.getItem('mostRecentScore')
-const feedback = document.querySelector('#feedback')
+const username = localStorage.getItem('playerName');
+const saveScoreBtn = document.querySelector('#saveScoreBtn');
+const finalScore = document.querySelector('#finalScore');
+const correctAnswers = document.querySelector('#correctAnswers');
+const numOfCorrectAnswers = localStorage.getItem("correctAnswers");
+const totalQuestions = localStorage.getItem("totalQuestions");
+const mostRecentScore= localStorage.getItem('mostRecentScore');
+const feedback = document.querySelector('#feedback');
 
 const highScores = JSON.parse(localStorage.getItem('highScores')) || []
 
 const MAX_HIGHSCORES = 5
 finalScore.innerText = mostRecentScore ?? 0
+correctAnswers.innerText = `${numOfCorrectAnswers} / ${totalQuestions}`;
 
 console.log(mostRecentScore)
 
 if (mostRecentScore == 0) {
     feedback.innerText = `Better luck next time, ${username}!`
 } else if (mostRecentScore > 0) {
-    feedback.innerText = `Congrats, ${username}!`
+    feedback.innerText = `Congratulations, ${username}!`
 }
 
 saveHighScore = e => {
