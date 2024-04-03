ad7c785b9ceba7168c3ce70d6e2cce9c82ae0ba1
burnjoe <sabanajholo@gmail.com>
2024-04-03 15:59:59 +0800

    fix leaderboard bug

--- javascript/highscores.js
+++ javascript/highscores.js
@@ -1,7 +1,7 @@
 const highScoresList = document.querySelector("#highScoresList")
 const highScores = JSON.parse(localStorage.getItem('highScores')) || []
-
-highScoresList.innerHTML = highScores === null ? 
+console.log(highScores === null)
+highScoresList.innerHTML = highScores !== null ? 
 highScores.map(score => {
     return  `<li class="high-score"> ${score.name} - ${score.score}  </li>`
 }).join('') :
